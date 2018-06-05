b923d42aea3f38f8d02927debe8d09e759220d22
Emily Welsch <emwelsch@gmail.com>
2018-06-05 17:28:54 +0000

    automatically backed up by learn

---
+++ lib/genre.rb
@@ -0,0 +1,26 @@
+class Genre
+  attr_accessor :name
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def create #instantiates an instance using .new but
+    #also invokes #save on that instance,
+    #forcing it to persist immediately.
+  end
+
+end
