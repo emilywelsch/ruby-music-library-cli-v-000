d5be0738f5eb6398a0ecc1fbe58bfd4c602f646e
Emily Welsch <emwelsch@gmail.com>
2018-06-05 17:47:25 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,7 +1,7 @@
 class Genre
   extend Concerns::Findable
   attr_accessor :name, :songs
-  
+
   @@all = []
 
   def self.all
@@ -10,7 +10,7 @@ class Genre
 
   def initialize(name)
     @name = name
-    songs = []
+    @songs = []
     #save
   end
 
