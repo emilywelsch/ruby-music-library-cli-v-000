d635b9871625651db107b7c2f3999edf0d7fba70
Emily Welsch <emwelsch@gmail.com>
2018-06-05 18:25:51 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -41,7 +41,7 @@ class MusicLibraryController
   end
 
   def list_artists # prints all artists in the music library in a numbered list (alphabetized by artist name)
-    Artist.all.sort{|a, b| a.name <=> b.name}.each.with_index(3) do |artist, index|
+    Artist.all.sort{|a, b| a.name <=> b.name}.each.with_index(1) do |artist, index|
       puts "#{index}. #{artist.name}"
     end
   end
