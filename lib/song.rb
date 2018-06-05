6c44abb9ed221f1b2cc627dd5c2a26f30b93275f
Emily Welsch <emwelsch@gmail.com>
2018-06-05 17:48:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,7 +22,7 @@ class Song
 
   def genre=(genre)
     @genre = genre
-    genre.sounds << self unless genre.songs.include?(self)
+    genre.songs << self unless genre.songs.include?(self)
   end
 
   def save
